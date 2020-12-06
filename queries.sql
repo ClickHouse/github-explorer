93c655561b9971dd1aeaa8cb4ac31c13f35e8227
Alexey Milovidov <milovidov@yandex-team.ru>
2020-12-07 02:08:11 +0300

    Updates for 2011..2014

--- queries.sql
+++ queries.sql
@@ -927,7 +927,7 @@ SELECT count(), repo_name FROM github_events WHERE event_type = 'WatchEvent' GRO
 
 -- Query 76:
 
-SELECT repo_name, count() FROM github_events WHERE event_type = 'WatchEvent' GROUP BY repo_name ORDER BY length(repo_name) ASC LIMIT 50;
+SELECT repo_name, count() FROM github_events WHERE event_type = 'WatchEvent' AND repo_name LIKE '%_/_%' GROUP BY repo_name ORDER BY length(repo_name) ASC LIMIT 50;
 
 
 -- Query 77:
